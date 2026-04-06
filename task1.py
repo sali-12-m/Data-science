839e32d4bca9f5f29cff2fb2812d3b64fb08fa8c
sali-12-m <salimmoh870@gmail.com>
2026-04-06 09:41:08 +0300

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,8 @@
+#reversing the string
+def rev_string(string):
+    #reverse the given string
+    return string[::-1]
+
+u_input=input("enter a string:")
+print("Reversed string")
+print(rev_string(u_input))
